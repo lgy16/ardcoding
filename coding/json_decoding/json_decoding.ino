576db285fdf1052edb2bf86bf89d22322676e648
Lee Gayoung <youngi2915@gmail.com>
2016-11-11 01:45:19 +0900

    parsing complete

--- coding/json_decoding/json_decoding.ino
+++ coding/json_decoding/json_decoding.ino
@@ -3,15 +3,47 @@
 void setup() {
   Serial.begin(9600);
 
-  char json[]="";
+  //char json[]= "{ \"url\" : \"/range\", \"method\" : \"PUT\", \"body\" : { \"device\" : \"motor\", \"state\" : 256 } }";
+  
+  char json[]= "{ \"url\" : \"/range\", \"method\" : \"GET\", \"body\" : { \"device\" :  \"motor\" } }";
+  
   StaticJsonBuffer<200> jsonBuffer;
   JsonObject & root = jsonBuffer.parseObject(json);
 
   const char* url = root ["url"];
   const char* method = root ["method"];
   
+  JsonObject & body = root ["body"];
+  const char* id = body ["id"];
+  const char* sensor = body ["sensors"];
+  const int value = body["value"];
+  const char* device = body["device"];
+  const int state = body["state"];
 
+  //url
+  Serial.print("url = ");
+  Serial.println(url);
 
+  //method
+  Serial.print("method = ");
+  Serial.println(method);
+
+  //body
+  //id
+  Serial.print("id = ");
+  Serial.println(id);
+  //sensor
+  Serial.print("sensor = ");
+  Serial.println(sensor);
+  //value
+  Serial.print("value = ");
+  Serial.println(value);
+  //device 
+  Serial.print("device = ");
+  Serial.println(device);
+  //state
+  Serial.print("state = ");
+  Serial.println(state);
 }
 
 void loop() {
