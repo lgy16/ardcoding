b99aa9da356e1f0d46914c9c099a3eafde646631
Lee Gayoung <youngi2915@gmail.com>
2016-11-22 14:47:52 +0900

    dasfasdfas

--- coding/sensor_ard4/sensor_ard4.ino
+++ coding/sensor_ard4/sensor_ard4.ino
@@ -13,12 +13,15 @@ void Post_Sensor_Data(String, double);
 void On_Off_Data(String);
 void Range_Data(String);
 void Autocheck_Sensor(int);
+void Json_Decoding();
 
 char ssid[] = "IIR_LAB"; //와이파이 이름
 char pass[] = "robot2015!"; //와이파이 비밀번호
+//char ssid[] = "Home"; //와이파이 이름
+//char pass[] = "01051216697"; //와이파이 비밀번호
 char server[] = "165.229.89.100";  //서버 IP주소
 int portnum = 443;
-int status = WL_IDLE_STATUS;
+int status;
 WiFiClient client;
 int sleeptime;
 
@@ -34,7 +37,10 @@ int count = 0;
 
 void setup() 
 {
+  Serial.println("setup()");
   Serial.begin(115200);
+
+   status = WL_IDLE_STATUS;
 
   // 현재 아두이노에 연결된 실드를 확인
   if (WiFi.status() == WL_NO_SHIELD) {
@@ -42,6 +48,7 @@ void setup()
     // 와이파이 실드가 아닐 경우
     while (true);
   }
+  
   // 와이파이 연결 시도
   while (status != WL_CONNECTED)
   {
@@ -60,10 +67,10 @@ void setup()
     Serial.println("connecting to Server...");
 
     client.println();
-    delay(10000);
+    delay(5000);
   }
-Serial.print("connected ");
-  sleeptime = 60;
+  Serial.print("connected ");
+  sleeptime = 20;
 
   pinMode(waterpumpPin, OUTPUT);
   pinMode(ledPin, OUTPUT);
@@ -74,26 +81,236 @@ Serial.print("connected ");
   Attatch_Devices();
   Serial.println();
 
-
-    Sensor_Data("light", analogRead(cdsPin));   //조도
-    Sensor_Data("temp", dht.readTemperature());  //온도
-    Sensor_Data("humi", dht.readHumidity());  //습도
-    Sensor_Data("soil", analogRead(SoilSensorPin));   //토양수분
-    Sensor_Data("gas", analogRead(GasSensorPin));   //가스
   led = 0;
   waterpump = "OFF";
   
   analogWrite(ledPin, led*50); //led 기본값 out
   digitalWrite(waterpumpPin, 0);  //waterpump 기본값 out
-
 }
-
 
 void loop()
 {
+  Serial.println("loop");
+ 
+  Json_Decoding();
+
+////////////////////////sensing///////////////////////
+  if(count==0 || count==sleeptime)
+  {
+    Serial.println("Sensing");
+    Sensor_Data("light", analogRead(cdsPin));   //조도
+    Sensor_Data("temp", dht.readTemperature());  //온도
+    Sensor_Data("humid", dht.readHumidity());  //습도
+    Sensor_Data("soil", analogRead(SoilSensorPin));   //토양수분
+    Sensor_Data("gas", analogRead(GasSensorPin));   //가스
+
+    count=count%sleeptime;
+  }
   
-  Serial.print("loop");
-  char json[256] {0};
+  delay(1000);
+  count++;
+}
+
+//////////////////////////json 함수들
+void Attatch_Arduino()
+{
+  Serial.print("Attatch_Arduino");
+  StaticJsonBuffer<200> jsonBuffer;
+
+  JsonObject& root = jsonBuffer.createObject();
+  root["url"] = "/arduino";
+  root["method"] = "PUT";
+  JsonObject& body = root.createNestedObject("body");
+  body["id"] = "84:38:35:6f:03:51";
+  body["name"] = "arduino2";
+
+  String buffer;
+  root.printTo(buffer);
+  Serial.println(buffer);
+  Serial.println(client.println(buffer.length()));
+  root.printTo(client);
+  //client.println(buffer);
+
+/*
+  int n = buffer.length()+1;
+  char buff[n];
+  buffer.toCharArray(buff, n);
+  Serial.println(buff);
+  //Serial.println(client.print(buff));
+  */
+}
+
+void Attatch_Devices()
+{
+  
+  Serial.print("Attatch_Devices");
+  StaticJsonBuffer<200> jsonBuffer;
+
+  JsonObject& root = jsonBuffer.createObject();
+  root["url"] = "/devices";
+  root["method"] = "PUT";
+  JsonObject& body = root.createNestedObject("body");
+  body["id"] = "84:38:35:6f:03:51";
+  JsonArray& devices = body.createNestedArray("devices");
+  devices.add("humid");
+  devices.add("soil");
+  devices.add("light");
+  devices.add("gas");
+  devices.add("led");
+  devices.add("pump");
+
+  String buffer;
+  root.printTo(buffer);
+  Serial.println(buffer);
+  Serial.println(client.println(buffer.length()));
+  root.printTo(client);
+  //client.println(buffer);
+/*
+  int n = buffer.length()+1;
+  char buff[n];
+  buffer.toCharArray(buff, n);
+  //Serial.println(buff);
+  //Serial.println(client.print(buff));
+  //tcp_send(buff);
+  */
+}
+
+void Sensor_Data(String sen, double value) 
+{
+  
+  //조도, 토양수분, 온습도, 가스
+  StaticJsonBuffer<200> jsonBuffer;
+
+  JsonObject& root = jsonBuffer.createObject();
+  root["url"] = "/sensors";
+  root["method"] = "PUT";
+  JsonObject& body = root.createNestedObject("body");
+  body["id"] = "84:38:35:6f:03:51";
+  body["sensor"] = sen;
+  body["value"] = value;
+/*
+  String buffer;
+  root.printTo(buffer);
+  Serial.println(buffer);
+  client.println(buffer.length());
+  //root.printTo(client);
+  int n = buffer.length()+1;
+  char buff[n];
+  buffer.toCharArray(buff, n);
+  //Serial.println(buff);
+  //Serial.println(client.print(buff));
+  tcp_send(buff);
+  */
+ 
+
+  String buffer;
+  root.printTo(buffer);
+  char *str;
+  root.printTo(str, buffer.length());
+  Serial.println(client.println(buffer.length()));
+  Serial.println(str);
+  for(int i=0; i<buffer.length(); i++)
+  {
+    client.write(str);
+  }
+  
+  
+/*
+  String json_string = "{\"url\":\"/sensors\",\"method\",:\"PUT\",\"body\":{\"id\":\"84:38:35:6f:03:50\",\"sensor\":\"" + sen + "\",\"value\":" + String(value) + "}}";
+
+  Serial.println(json_string);
+  Serial.println(client.println(json_string.length()));
+  //Serial.println(client.println(json_string));
+  int n = json_string.length()+1;
+  char buff[n];
+  json_string.toCharArray(buff, n);
+  Serial.println(buff);
+  Serial.println(client.print("{\"url\":\"/sensors\",\"method\",:\"PUT\",\"body\":{\"id\":\"84:38:35:6f:03:50\",\"sensor\":\"" + sen + "\",\"value\":" + String(value) + "}}"));
+  */
+}
+
+void Post_Sensor_Data(String sen, double value) 
+{
+  StaticJsonBuffer<200> jsonBuffer;
+
+  JsonObject& root = jsonBuffer.createObject();
+  root["url"] = "/sensors";
+  root["method"] = "POST";
+  JsonObject& body = root.createNestedObject("body");
+  body["id"] = "84:38:35:6f:03:51";
+  body["sensor"] = sen;
+  body["value"] = value;
+
+  String buffer;
+  root.printTo(buffer);
+  client.println(buffer.length());
+  //root.printTo(client);
+  client.println(buffer);
+}
+
+void  On_Off_Data(String device) 
+{  
+  //waterpump
+  //fan, pump
+  StaticJsonBuffer<200> jsonBuffer;
+
+  JsonObject& root = jsonBuffer.createObject();
+  root["url"] = "/onoff";
+  root["method"] = "Post";
+  JsonObject& body = root.createNestedObject("body");
+  body["id"] = "84:38:35:6f:03:51";
+  body["device"] = device;
+  body["state"] = waterpump;
+
+  String buffer;
+  root.printTo(buffer);
+  client.println(buffer.length());
+  //root.printTo(client);
+  client.println(buffer);
+}
+
+void Range_Data(String device)  // led
+{
+  StaticJsonBuffer<200> jsonBuffer;
+
+  JsonObject& root = jsonBuffer.createObject();
+  root["url"] = "/range";
+  root["method"] = "POST";
+  JsonObject& body = root.createNestedObject("body");
+  body["id"] = "84:38:35:6f:03:51";
+  body["device"] = device;
+  body["state"] = led;
+
+  String buffer;
+  root.printTo(buffer);
+  client.println(buffer.length());
+  //root.printTo(client);
+  client.println(buffer);
+}
+
+void Autocheck_Sensor(int duration)
+{
+  StaticJsonBuffer<200> jsonBuffer;
+
+  JsonObject& root = jsonBuffer.createObject();
+  root["url"] = "/duration";
+  root["method"] = "POST";
+  JsonObject& body = root.createNestedObject("body");
+  body["id"] = "84:38:35:6f:03:51";
+  body["second"] = duration;
+
+  String buffer;
+  root.printTo(buffer);
+  client.println(buffer.length());
+  //root.printTo(client);
+  client.println(buffer);
+}
+
+
+void Json_Decoding()
+{
+  Serial.println("Json Decoding");
+   char json[256] {0};
   client.read((uint8_t*)json, 256);
 
   Serial.println(json);
@@ -237,163 +454,4 @@ void loop()
     count = 0;
     Autocheck_Sensor(sleeptime);
   }
-
-
-
-////////////////////////sensing///////////////////////
-  if(count==0 || count==sleeptime)
-  {
-    Sensor_Data("light", analogRead(cdsPin));   //조도
-    Sensor_Data("temp", dht.readTemperature());  //온도
-    Sensor_Data("humi", dht.readHumidity());  //습도
-    Sensor_Data("soil", analogRead(SoilSensorPin));   //토양수분
-    Sensor_Data("gas", analogRead(GasSensorPin));   //가스
-
-    count=count%sleeptime;
-  }
-  
-  delay(1000);
-  count++;
-}
-
-
-
-
-//////////////////////////json 함수들
-void Attatch_Arduino()
-{
-  Serial.print("Attatch_Arduino");
-  StaticJsonBuffer<200> jsonBuffer;
-
-  JsonObject& root = jsonBuffer.createObject();
-  root["url"] = "/arduino";
-  root["method"] = "PUT";
-  JsonObject& body = root.createNestedObject("body");
-  body["id"] = "84:38:35:6f:03:51";
-  body["name"] = "arduino2";
-
-  String buffer;
-  root.printTo(buffer);
-  Serial.println(buffer);
-  client.print(buffer.length());
-  root.printTo(client);
-}
-
-void Attatch_Devices()
-{
-  
-  Serial.print("Attatch_Devices");
-  StaticJsonBuffer<200> jsonBuffer;
-
-  JsonObject& root = jsonBuffer.createObject();
-  root["url"] = "/devices";
-  root["method"] = "PUT";
-  JsonObject& body = root.createNestedObject("body");
-  body["id"] = "84:38:35:6f:03:51";
-  JsonArray& devices = body.createNestedArray("devices");
-  devices.add("humid");
-  devices.add("soil");
-  devices.add("light");
-  devices.add("gas");
-  devices.add("led");
-  devices.add("pump");
-
-  String buffer;
-  root.printTo(buffer);
-  Serial.println(buffer);
-  client.print(buffer.length());
-  root.printTo(client);
-}
-
-void Sensor_Data(String sen, double value) 
-{
-  //조도, 토양수분, 온습도, 가스
-  StaticJsonBuffer<200> jsonBuffer;
-
-  JsonObject& root = jsonBuffer.createObject();
-  root["url"] = "/sensors";
-  root["method"] = "PUT";
-  JsonObject& body = root.createNestedObject("body");
-  body["id"] = "84:38:35:6f:03:51";
-  body["sensor"] = sen;
-  body["value"] = value;
-
-  String buffer;
-  root.printTo(buffer);
-  Serial.println(buffer);
-  client.print(buffer.length());
-  root.printTo(client);
-}
-
-void Post_Sensor_Data(String sen, double value) 
-{
-  StaticJsonBuffer<200> jsonBuffer;
-
-  JsonObject& root = jsonBuffer.createObject();
-  root["url"] = "/sensors";
-  root["method"] = "POST";
-  JsonObject& body = root.createNestedObject("body");
-  body["id"] = "84:38:35:6f:03:51";
-  body["sensor"] = sen;
-  body["value"] = value;
-
-  String buffer;
-  root.printTo(buffer);
-  client.print(buffer.length());
-  root.printTo(client);
-}
-
-void  On_Off_Data(String device) 
-{  
-  //waterpump
-  //fan, pump
-  StaticJsonBuffer<200> jsonBuffer;
-
-  JsonObject& root = jsonBuffer.createObject();
-  root["url"] = "/onoff";
-  root["method"] = "Post";
-  JsonObject& body = root.createNestedObject("body");
-  body["id"] = "84:38:35:6f:03:51";
-  body["device"] = device;
-  body["state"] = waterpump;
-
-  String buffer;
-  root.printTo(buffer);
-  client.print(buffer.length());
-  root.printTo(client);
-}
-
-void Range_Data(String device)  // led
-{
-  StaticJsonBuffer<200> jsonBuffer;
-
-  JsonObject& root = jsonBuffer.createObject();
-  root["url"] = "/range";
-  root["method"] = "POST";
-  JsonObject& body = root.createNestedObject("body");
-  body["id"] = "84:38:35:6f:03:51";
-  body["device"] = device;
-  body["state"] = led;
-
-  String buffer;
-  root.printTo(buffer);
-  client.print(buffer.length());
-  root.printTo(client);
-}
-
-void Autocheck_Sensor(int duration)
-{
-  StaticJsonBuffer<200> jsonBuffer;
-
-  JsonObject& root = jsonBuffer.createObject();
-  root["url"] = "/duration";
-  root["method"] = "POST";
-  JsonObject& body = root.createNestedObject("body");
-  body["id"] = "84:38:35:6f:03:51";
-  body["second"] = duration;
-
-  String buffer;
-  root.printTo(buffer);
-  client.print(buffer.length());
-  root.printTo(client);
 }
